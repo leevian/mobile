5ad65493231fd3f24c804b0e1cd84d1f22ece567
zhangtinghua <zhangtinghua@shopex.cn>
2016-07-18 19:20:36 +0800

    bug

--- dls/js/common.js
+++ dls/js/common.js
@@ -1,30 +1,7 @@
- $(function(){
-    var audio = $('#audio')[0]
-
-    var musicLogo = $(".music-logo");
-
-    var isStart = false;
-
-   $(musicLogo).bind('touchend',function () {
-        if ( isStart == false ) {
-
-            $(musicLogo).addClass( "playing" );
-            audio.play();
-        }
-
-        isStart = true;
-
-    } );
-    $(musicLogo).bind('touchend', function () {
-        // musicLogo.hasClass( "playing" ) ? audio.pause();musiclogo.removeClass('playing') : audio.play();musiclogo.addClass('playing');
-        // musicLogo.toggle( "playing" );
-        if($(musicLogo).hasClass( "playing" )){
-            audio.pause();
-            $(musicLogo).removeClass('playing')
-        }else{
-             audio.play();
-             $(musicLogo).addClass('playing')
-        }
-    } );
- })   
-    
+$(function(){
+    $('.pop-overlay .close-js a').bind('touchend',function(e){
+        e.stopPropagation();
+        $(this).parents('.pop-overlay').hide();
+        return false;
+    })
+})
