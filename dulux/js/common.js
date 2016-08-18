8671a5a92be2cbdac0513c407b3c3e0447ff2c41
zhangtinghua <zhangtinghua@shopex.cn>
2016-08-18 17:57:11 +0800

    deluxe

--- dulux/js/common.js
+++ dulux/js/common.js
@@ -1,6 +1,6 @@
 $(function(){
-    $('.pop-overlay .close-btn a').bind('touchend',function(e){
-        e.stopPropagation();
+    $('.pop-overlay .close-btn').bind('touchend',function(e){
+  
         $(this).parents('.pop-overlay').css('z-index','1');
         return false;
     })
